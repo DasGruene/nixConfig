ae87f6f8f5475507ec839631d6631fd2bb7bfbfa
DasGruene <caspergronnehotmail.com>
2025-11-16 13:12:44 +0100

    updates nvim and cleans up config file

--- configuration.nix
+++ configuration.nix
@@ -2,16 +2,20 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, kickstart-nixvim, ... }:
-
+{
+  config,
+  pkgs,
+  kickstart-nixvim,
+  ...
+}:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      kickstart-nixvim.nixosModules.default
-    ];
-  
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    kickstart-nixvim.nixosModules.default
+  ];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 10;
@@ -49,9 +53,9 @@
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
-  
+
   programs.nm-applet.enable = true;
-  
+
   #Hyprland stuff
   services.hypridle.enable = true;
   programs.hyprland = {
@@ -62,22 +66,21 @@
 
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
-    TERMINAL = "ghostty"; #makes nvim and vim open in ghostty if called through the runner
+    TERMINAL = "ghostty"; # makes nvim and vim open in ghostty if called through the runner
     #NIXOS_OZONE_WL = "1";
   };
 
-
   hardware = {
-      graphics.enable = true;
-      graphics.enable32Bit = true;
-      graphics.extraPackages = with pkgs; [ 
-        vpl-gpu-rt  
-        nvidia-vaapi-driver
-      ];
+    graphics.enable = true;
+    graphics.enable32Bit = true;
+    graphics.extraPackages = with pkgs; [
+      vpl-gpu-rt
+      nvidia-vaapi-driver
+    ];
   };
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
 
@@ -86,7 +89,7 @@
 
     # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
     # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
     # of just the bare essentials.
     powerManagement.enable = false;
 
@@ -96,25 +99,25 @@
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     open = false;
 
     # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
+    # accessible via `nvidia-settings`.
     nvidiaSettings = false;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
 
     prime = {
-	sync.enable = true;
+      sync.enable = true;
 
-	nvidiaBusId = "PCI:0:1:0";
-	intelBusId = "PCI:0:2:0";
-	};
+      nvidiaBusId = "PCI:0:1:0";
+      intelBusId = "PCI:0:2:0";
+    };
   };
 
   boot.blacklistedKernelModules = [ "nouveau" ];
@@ -123,7 +126,7 @@
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.theme = "where-is-my-sddm-theme";
   services.displayManager.sddm.wayland.enable = true;
-#  services.desktopManager.plasma6.enable = true;
+  #  services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -155,27 +158,32 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
-  
+
   #virrual machine stuff:
   programs.virt-manager.enable = true;
-  users.groups.libvirtd.members = ["your_username"];
+  users.groups.libvirtd.members = [ "your_username" ];
   virtualisation.spiceUSBRedirection.enable = true;
   virtualisation.libvirtd = {
     enable = true;
     qemu.vhostUserPackages = with pkgs; [ virtiofsd ];
   };
-  
-  services.udisks2.enable = true; #auto maount usbs
+
+  services.udisks2.enable = true; # auto maount usbs
   boot.supportedFilesystems = [ "ntfs" ]; # allow winodws format storages devices
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.user = {
     isNormalUser = true;
     description = "user";
-    extraGroups = [ "networkmanager" "wheel" "libvirtd" "docker"];
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "libvirtd"
+      "docker"
+    ];
     packages = with pkgs; [
       kdePackages.kate
-    #  thunderbird
+      #  thunderbird
     ];
   };
 
@@ -183,7 +191,6 @@
   nixpkgs.config.allowUnfree = true;
   programs.direnv.enable = true;
   virtualisation.docker.enable = true;
-
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -204,22 +211,25 @@
     spotify
     unzip
     qmk
-    pavucontrol #audio
+    pavucontrol # audio
     kitty # default terminal hyprland
     waybar # bar for hyprland
     font-awesome # font used by waybar
-    rofi-wayland #program runner hyprland
+    rofi-wayland # program runner hyprland
     lshw # used to get graphic card info
     macchina # fetches system info
     fastfetch
     qdirstat # disk analyser
-    networkmanagerapplet #network ui
-    imagemagick #to adjust image sizes
-    hyprpaper #wallpaper
-    alsa-utils #sound util UI
-    xfce.thunar-archive-plugin #archive addon to thunar
+    networkmanagerapplet # network ui
+    imagemagick # to adjust image sizes
+    hyprpaper # wallpaper
+    alsa-utils # sound util UI
+    xfce.thunar-archive-plugin # archive addon to thunar
     weston # used by sddm
+    tree # make it posible to print folder trees in terminal
     where-is-my-sddm-theme
+    opencode
+    warp-terminal
     wineWowPackages.stable
 
     # support 32-bit only
@@ -241,73 +251,74 @@
     wineWowPackages.waylandFull
     (vscode-with-extensions.override {
       vscode = vscode;
-      vscodeExtensions = with vscode-extensions; [
-        #puplisher.extension
-	bbenoist.nix
-        ms-python.python
-        ms-pyright.pyright
-        njpwerner.autodocstring
-	vscodevim.vim
-	yzhang.markdown-all-in-one
-	mkhl.direnv
-      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-## Link to info: https://raw.githubusercontent.com/nix-community/nix-vscode-extensions/refs/heads/master/data/cache/vscode-marketplace-latest.json
-        {
-          name = "elixir-ls";
-          publisher = "JakeBecker";
-          version = "0.28.0";
-          sha256 = "sha256-pHLAA7i2HJC523lPotUy5Zwa3BTSTurC2BA+eevdH38=";
-        }
-	{
-          name = "python-extension-pack";
-          publisher = "LeoJhonSong";
-          version = "3.0.0";
-          sha256 = "sha256-SfA+i2BD7xlELLmC8/mThZFwI0n86+/HnUZWNW5rb1s=";
-        }
-        {
-          name = "ts-error-translator";
-          publisher = "mattpocock";
-          version = "0.10.1";
-          sha256 = "sha256-WBdtRFaGKUmsriwUgNRToaqGJ6sdzrvOMs/fhEQFmws=";
-        }
-	{
-          name = "remote-containers";
-          publisher = "ms-vscode-remote";
-          version = "0.424.0";
-          sha256 = "sha256-BI7zZVSebatslFUkynr3eY3pRigbDBcpNG1JAOBGrBE=";
-        }
-      ]; 
+      vscodeExtensions =
+        with vscode-extensions;
+        [
+          #puplisher.extension
+          bbenoist.nix
+          ms-python.python
+          ms-pyright.pyright
+          njpwerner.autodocstring
+          vscodevim.vim
+          yzhang.markdown-all-in-one
+          mkhl.direnv
+        ]
+        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          ## Link to info: https://raw.githubusercontent.com/nix-community/nix-vscode-extensions/refs/heads/master/data/cache/vscode-marketplace-latest.json
+          {
+            name = "elixir-ls";
+            publisher = "JakeBecker";
+            version = "0.28.0";
+            sha256 = "sha256-pHLAA7i2HJC523lPotUy5Zwa3BTSTurC2BA+eevdH38=";
+          }
+          {
+            name = "python-extension-pack";
+            publisher = "LeoJhonSong";
+            version = "3.0.0";
+            sha256 = "sha256-SfA+i2BD7xlELLmC8/mThZFwI0n86+/HnUZWNW5rb1s=";
+          }
+          {
+            name = "ts-error-translator";
+            publisher = "mattpocock";
+            version = "0.10.1";
+            sha256 = "sha256-WBdtRFaGKUmsriwUgNRToaqGJ6sdzrvOMs/fhEQFmws=";
+          }
+          {
+            name = "remote-containers";
+            publisher = "ms-vscode-remote";
+            version = "0.424.0";
+            sha256 = "sha256-BI7zZVSebatslFUkynr3eY3pRigbDBcpNG1JAOBGrBE=";
+          }
+        ];
     })
   ];
 
-
   programs.nixvim.enable = true;
   programs.nixvim.defaultEditor = true;
-  
 
   programs.thunar.enable = true;
   programs.xfconf.enable = true;
   xdg.mime.enable = true;
   xdg.mime.defaultApplications = {
-        "inode/directory" = "thunar.desktop";
-        "application/x-gnome-saved-search" = "thunar.desktop";
+    "inode/directory" = "thunar.desktop";
+    "application/x-gnome-saved-search" = "thunar.desktop";
   };
-  
+
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
   ];
 
-  xdg.portal = { 
+  xdg.portal = {
     enable = true;
     xdgOpenUsePortal = true;
-    
+
     config.common.default = [
       "hyprland"
       #"gtk"
     ];
 
     # systemctl --user status xdg-desktop-portal-hyprland.service
-    extraPortals = [ 
+    extraPortals = [
       pkgs.xdg-desktop-portal-hyprland
       #pkgs.xdg-desktop-portal-gtk
     ];
@@ -318,7 +329,7 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  }; 
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -346,7 +357,7 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
-  
+
   #auto clean of nixos
   nix.gc = {
     automatic = true;
@@ -356,5 +367,8 @@
 
   system.autoUpgrade.enable = true;
   nix.settings.max-jobs = 8;
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 }
---
+++ nix-rebuild.sh
@@ -0,0 +1,1 @@
+sudo nixos-rebuild switch --flake ./#user --show-trace
